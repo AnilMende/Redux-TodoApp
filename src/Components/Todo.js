4d6ce7bf05c0f32d0ae271894dc1eb9e11a7d6bc
Anil <anilmende2002@email.com>
2025-09-05 13:19:29 +0530

    v2

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,11 +1,21 @@
 import React from "react";
-
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 const Todo = () => {
     return(
         <>
             <div className="todo_data col-lg-5 mx-auto mt-2">
-                <div className="todo_container" style={{background:"#dcdadaff", borderRadius:"3px", height:"45px"}}>
+                <div className="todo_container d-flex justify-content-between align-items-center px-2" 
+                                style={{background:"#dcdadaff", borderRadius:"3px", height:"45px"}}>
+
                     <li style={{listStyle:"none"}}>React</li>
+
+                    <div className="edit_dlt col-lg-3 py-2 d-flex justify-content-between align-items-center">
+                        <EditIcon style={{color:"blue", cursor:"pointer"}}/>
+                        <DeleteIcon style={{color:"red", cursor:"pointer"}}/>
+                        <RemoveRedEyeIcon style={{color:"#1dd1a1", cursor:"pointer"}}/>
+                    </div>
                 </div>
             </div>
         </>
